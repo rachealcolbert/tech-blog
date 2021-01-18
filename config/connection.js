e0c469c794f7151b846757b4201e9f83ffdbc2b7
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 01:16:35 -0700

    changes to server can't connect yet

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const Sequelize = require('sequilize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
-// create connection to db 
-const sequelize = process.env.JAWSBD_URL ?
-    new Sequelize(process.env.JAWSBD_URL) :
-    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'local host',
-        dialect: 'mysql',
-        port: 3306
+// create connection to db
+const sequelize = process.env.JAWSBD_URL
+  ? new Sequelize(process.env.JAWSBD_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
     });
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,46 +1,51 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const {
-    userInfo
-} = require('os');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
 
 // get all posts for home
 
-router.get('/', (req, res) => {
-    console.log('================');
-    Post.findAll({
-            attributes: [
-                'id',
-                'post_url',
-                'create_at',
-                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-            ],
-            include: [{
-                    model: Comment,
-                    attributed: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
+router.get("/", (req, res) => {
+  console.log("================");
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "create_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) =>
+        post.get({
+          plain: true,
         })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({
-                plain: true
-            }));
+      );
 
-            res.render('homepage', {
-                posts,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
