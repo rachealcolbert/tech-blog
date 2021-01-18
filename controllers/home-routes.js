d3ded7b2ab6d634072582260e26030e562ac4d46
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 12:22:57 -0700

    homepage  view added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
+const {
+  Post
+} = require('../models')
 
 // get all posts for home
 
@@ -7,19 +10,19 @@ router.get("/", (req, res) => {
   console.log("================");
   Post.findAll({
       attributes: ["id", "post_text", "create_at"],
-      include: [{
-          model: Comment,
-          attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
-          model: User,
-          attributes: ["username"],
-        },
-      ],
+      // include: [{
+      //     model: Comment,
+      //     attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
+      //     include: {
+      //       model: User,
+      //       attributes: ["username"],
+      //     },
+      //   },
+      //   {
+      //     model: User,
+      //     attributes: ["username"],
+      //   },
+      // ],
     })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) =>
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
-const seedUsers = require('./user-seeds');
+// const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
-const seedComments = require('./comment-seeds');
+// const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -8,15 +8,15 @@ const seedAll = async () => {
     await sequelize.sync({
         force: true
     });
-    console.log('--------------');
-    await seedUsers();
-    console.log('--------------');
+    // console.log('--------------');
+    // await seedUsers();
+    // console.log('--------------');
 
     await seedPosts();
     console.log('--------------');
 
-    await seedComments();
-    console.log('--------------');
+    // await seedComments();
+    // console.log('--------------');
 
     process.exit(0);
 };
