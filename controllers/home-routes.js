bd2d897c16d76c6b88c6c0a8078e5a6066fc8607
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 13:01:58 -0700

    changes to home-route.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,29 @@
-const router = require("express").Router();
-// const sequelize = require("../config/connection");
-// const {
-//   Post
-// } = require('../models')
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {
+  Post
+} = require('../models');
 
-// get all posts for home
-
-router.get("/", (req, res) => {
-  res.render('homepage');
+// get all users
+router.get('/', (req, res) => {
+  Post.findAll({
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+    })
+    .then(dbPostData => {
+      console.log(dbPostData[0]);
+      res.render('homepage', dbPostData[0].get({
+        plain: true
+      }));
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 module.exports = router;
