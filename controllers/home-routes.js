11cf2d56628664ddb6c3587e41cd7200230ff656
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 12:46:32 -0700

    restarted home-routes get to just render page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,45 +1,12 @@
 const router = require("express").Router();
-const sequelize = require("../config/connection");
-const {
-  Post
-} = require('../models')
+// const sequelize = require("../config/connection");
+// const {
+//   Post
+// } = require('../models')
 
 // get all posts for home
 
 router.get("/", (req, res) => {
-  console.log("================");
-  Post.findAll({
-      attributes: ["id", "post_text", "create_at"],
-      // include: [{
-      //     model: Comment,
-      //     attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
-      //     include: {
-      //       model: User,
-      //       attributes: ["username"],
-      //     },
-      //   },
-      //   {
-      //     model: User,
-      //     attributes: ["username"],
-      //   },
-      // ],
-    })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) =>
-        post.get({
-          plain: true,
-        })
-      );
-
-      res.render("homepage", {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.render('homepage');
 });
-
 module.exports = router;
