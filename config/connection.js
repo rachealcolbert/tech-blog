6b0dc1548606a4975753218294af5486d1960ac7
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 12:13:38 -0700

    change server name to connect

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,19 @@ const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
+console.log("DB_NAME: ", process.env.DB_NAME);
+console.log("DB_USER: ", process.env.DB_USER);
+console.log("DB_PW: ", process.env.DB_PW);
+
 // create connection to db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequelize = process.env.JAWSDB_URL ?
+    new Sequelize(process.env.JAWSDB_URL) :
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    });
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,32 +6,21 @@ const sequelize = require("../config/connection");
 router.get("/", (req, res) => {
   console.log("================");
   Post.findAll({
-    attributes: [
-      "id",
-      "post_url",
-      "create_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
+      attributes: ["id", "post_text", "create_at"],
+      include: [{
+          model: Comment,
+          attributed: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
           model: User,
           attributes: ["username"],
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
+      ],
+    })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) =>
         post.get({
